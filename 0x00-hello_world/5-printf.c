6b747020efa21a04451dab77da74580357725637
Chinemerem390 <chinemesamuelo@gmail.com>
2022-09-08 20:26:05 -0700

    Write a C program that prints exactly with proper grammar, but the outcome is a piece of art,, followed by a new line.

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /*
- * main - print the string in puts function
+ * main- print the string in puts function
  *
  * Descriotion: Using the main function
  *  print with proper grammar, but the outcome is a piece of art,
