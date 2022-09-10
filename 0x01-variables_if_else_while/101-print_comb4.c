b9c0f6ef5bd44c1d9272ce83e18764511adbf9a8
Chinemerem390 <chinemesamuelo@gmail.com>
2022-09-09 21:47:36 -0700

    putchar(,);

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - printing numbers
+ * Description: Writes all unique combination of 
+ * two numbers
+ * * Return: 0 for success
+ */
+int main(void)
+{
+int i, j;
+for (i = '0'; i <= '9'; i++)
+{
+for (j = '0'; j <= '9'; j++)
+{
+if ((i < j) & (j <= '9'))
+{
+putchar(i);
+putchar(j);
+if ((j < '9') | (i < '8'))
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
