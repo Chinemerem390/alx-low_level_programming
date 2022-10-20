990fe1140e310af2a76abc567c43a494a72d49d3
Igboticcoder <89645939+Chinemerem390@users.noreply.github.com>
2022-10-20 20:50:55 +0100

    Update 101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,15 +1,9 @@
-ECTION .data
-msg:	db "Hello, Holberton", 0
-fmt:	db "%s", 10, 0
-
-	SECTION .text
-	extern printf
-	global main
+global    main
+          extern    printf
 main:
-	mov esi, msg
-	mov edi, fmt
-	mov eax, 0
-	call printf
-
-	mov eax, 0
-	ret
+	  mov   edi, format
+	  xor   eax, eax
+	  call  printf
+	  mov 	eax, 0
+	  ret
+format: db `Hello, Holberton\n`,0
